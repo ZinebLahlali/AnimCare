45c75a72717fb7bdf4d2f5837451874de10bfc40
Zineb <zeynepturna1992@gmail.com>
2026-04-30 12:57:02 +0100

    feat(admin): add static function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Animal;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
-{
-    
+{   
+    public function total()
+    {
+        $totalOwner = User::where('role', 'Owner')->count();
+        $totalVet = User::where('role', 'Vet')->count();
+        $totalAnimals = Animal::count();
+
+        return view('admin.dashboard', compact('totalOwner', 'totalVet', 'totalAnimals'));
+
+    }
+
+   
+
+ 
+
+
 }
